db2b9ffd10e14ae799c10324f3b60be9051d4842
annngure <annwanjikungure@gmail.com>
2021-11-12 15:18:39 +0300

    add male female days

---
+++ operations.js
@@ -0,0 +1,8 @@
+const male=["Kwasi", "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"]
+   console.log( male);
+
+ const female=["Akasua", "Adwoa" ,"Abenna" ,"Akua" ,"Yaa" ,"Afua" ,"Ama"]
+  console.log ( female);
+
+  const days=["Sunday" ,"Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday"]
+  console.log(days);
