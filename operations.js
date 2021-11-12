88878c05b9a16e4e2823a4590c29635450586367
annngure <annwanjikungure@gmail.com>
2021-11-12 16:25:39 +0300

    add conditions of java

--- operations.js
+++ operations.js
@@ -5,4 +5,30 @@ const male=["Kwasi", "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"]
   console.log ( female);
 
   const days=["Sunday" ,"Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday"]
-  console.log(days);
+  console.log(days); 
+
+    switch (true){
+        case( male[0]= days[0]):
+        Name="Kwasi"
+        break;
+        case(male[1]=days[1]):
+        Name="Kwadwo"
+        break;
+        case(male [2]=days[2]):
+        Name="Kwabena"
+        break;
+        case(male[3]=days[3]):
+        Name="Kwaku"
+        break;
+        case(male[4]=days[4]):
+        Name="Yaw"
+        break;
+        case(male[5]=days[5]):
+        Name="Kofi"
+        break;
+        case (male[6]=days[6]):
+        Name="Kwame"
+        break;
+        default:
+        Name="Invalid"
+    } 
